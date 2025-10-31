6a3f414d8c815dcc5bda88f7c8797665123ce8f5
isher2k1 <ikekches@gmail.com>
2025-10-31 20:54:17 +0200

    implement the StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,38 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final List<K> keyList;
+    private final List<V> valueList;
+
+    public StorageImpl() {
+        keyList = new ArrayList<>();
+        valueList = new ArrayList<>();
+    }
+
     @Override
     public void put(K key, V value) {
+        if (!keyList.contains(key)) {
+            keyList.add(key);
+            valueList.add(value);
+        } else {
+            valueList.set(keyList.indexOf(key), value);
+        }
     }
 
     @Override
     public V get(K key) {
+        if (keyList.contains(key)) {
+            return valueList.get(keyList.indexOf(key));
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keyList.size();
     }
 }
