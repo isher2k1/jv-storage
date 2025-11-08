bb6ee5cfc0aeee70e1b7ab08313dffa16d273a9e
isher2k1 <ikekches@gmail.com>
2025-11-08 19:06:55 +0200

    fix issues

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,38 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final List<K> keyList;
-    private final List<V> valueList;
+    private static final int ARRAY_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int index;
 
     public StorageImpl() {
-        keyList = new ArrayList<>();
-        valueList = new ArrayList<>();
+        Object[] genericKeys = new Object[ARRAY_SIZE];
+        Object[] genericValues = new Object[ARRAY_SIZE];
+
+        this.keys = (K[]) genericKeys;
+        this.values = (V[]) genericValues;
+        this.index = 0;
     }
 
-    @Override
     public void put(K key, V value) {
-        if (!keyList.contains(key)) {
-            keyList.add(key);
-            valueList.add(value);
-        } else {
-            valueList.set(keyList.indexOf(key), value);
+        for (int i = 0; i < index; i++) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        if (index != ARRAY_SIZE) {
+            keys[index] = key;
+            values[index] = value;
+            index++;
         }
     }
 
     @Override
     public V get(K key) {
-        if (keyList.contains(key)) {
-            return valueList.get(keyList.indexOf(key));
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
         }
         return null;
     }
 
     @Override
     public int size() {
-        return keyList.size();
+        return index;
     }
 }
